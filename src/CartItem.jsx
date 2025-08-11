9d762299abbcdd187692b70a9ed52fefb53e5f4c
Spandana1999 <smorampudi1999@gmail.com>
2025-08-11 15:18:32 -0500

    added cart page

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React from "react";
 
-function CartItem() {
+import "./CartItem.css";
+
+const CartItem = ({ onContinueShopping }) => {
+
+  const calculateTotalAmount = () => {
+    
+  };
+
+  const handleContinueShopping = (e) => {
+    onContinueShopping(e);
+  };
+
+
+ 
+
+  const handleCheckoutClick = () => {
+    window.alert("The website is under development.");
+  };
+
   return (
-    <div>
+    <div className="cart-container">
+      <h2 style={{ color: "black" }}>
+        Total Cart Amount: ${calculateTotalAmount()}
+      </h2>
+      <div>
       
+        Cart Page
+      </div>
+      <div
+        style={{ marginTop: "20px", color: "black" }}
+        className="total_cart_amount"
+      ></div>
+      <div className="continue_shopping_btn">
+        <button
+          className="get-started-button1"
+          onClick={(e) => handleContinueShopping(e)}
+        >
+          Continue Shopping
+        </button>
+        <br />
+        <button className="get-started-button1" onClick={handleCheckoutClick}>
+          Checkout
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CartItem
+export default CartItem;
