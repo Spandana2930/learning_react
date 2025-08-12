1c8da8e528e4dcc689d021fd546fa8e49be1ffde
Spandana1999 <smorampudi1999@gmail.com>
2025-08-12 15:43:10 -0500

    fixed landing page issue

--- src/App.jsx
+++ src/App.jsx
@@ -5,38 +5,46 @@ import AboutUs from "./AboutUs";
 import NavProductList from "./NavProductList";
 
 function App() {
-  const [showVenue, setShowVenue] = useState(false);
+   const [showProductList, setShowProductList] = useState(false);
 
   const handleGetStarted = () => {
-    setShowVenue(true);
+    setShowProductList(true);
+  };
+   const handleHomeClick = () => {
+    setShowProductList(false);
   };
 
   return (
-    <>
-     <div className="background-image"></div>
-      <header className="first_page">
-        <div className="main_event">
-            
-          <div className="first_page_name_btn">
-            <h1 className="budget_heading">Welcome to Paradise Nursery</h1>
-            <p className="budget_sentence"> Where Green Meets Serenity</p>
-            <div className="getstarted_btn">
-              <button onClick={() => handleGetStarted()} className="get-started-btn">
-                Get Started
-              </button>
+  <>
+    {showProductList ? (
+      <NavProductList onHomeClick={handleHomeClick} />
+    ) : (
+      <>
+        <div className="background-image"></div>
+        <header className="first_page">
+          <div className="main_event">
+            <div className="first_page_name_btn">
+              <h1 className="budget_heading">Welcome to Paradise Nursery</h1>
+              <p className="budget_sentence">Where Green Meets Serenity</p>
+              <div className="getstarted_btn">
+                <button
+                  onClick={handleGetStarted}
+                  className="get-started-btn"
+                >
+                  Get Started
+                </button>
+              </div>
+            </div>
+            <div className="aboutus_main">
+              <AboutUs />
             </div>
           </div>
-          <div className="aboutus_main">
-            <AboutUs />
-          </div>
-        </div>
-      </header>
+        </header>
+      </>
+    )}
+  </>
+);
 
-      <div className={`event-list-container ${showVenue ? 'visible' : ''}`}>
-        <NavProductList/>
-      </div>
-    </>
-  );
 }
 
 export default App;
